2f7614b12314fd274f2bbbbc2f41349bc649f535
Dhruva Kumar <102192960+subbuguru@users.noreply.github.com>
2024-01-05 17:25:18 -0600

    Create camera_helper.dart
    Initial class to aid with saving medication photo

---
+++ lib/local_service/camera_helper.dart
@@ -0,0 +1,27 @@
+// camera_helper.dart
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+
+class CameraHelper {
+  static Future<String?> captureAndSaveImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.camera);
+
+    if (image != null) {
+      return _saveImagePermanently(image);
+    }
+    return null;
+  }
+
+  static Future<String> _saveImagePermanently(XFile image) async {
+    final Directory appDocDir = await getApplicationDocumentsDirectory();
+    final String appDocPath = appDocDir.path;
+    final String fileName = path.basename(image.path);
+    final String localPath = path.join(appDocPath, fileName);
+
+    await File(image.path).copy(localPath);
+    return localPath;
+  }
+}
