e3dc1b42084596f9fa950f61ff6d573c1f259f88
Dhruva Kumar <102192960+subbuguru@users.noreply.github.com>
2024-01-06 21:05:46 -0600

    disable debug thing

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      //showSemanticsDebugger: false,
       theme: ThemeData(
         //... theme data ...
         colorScheme: const ColorScheme.light(
