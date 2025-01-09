7ee1c14cc57a43219514d9665a41f4917d3ed36d
Dhruva Kumar <unkumar28@gmail.com>
2025-01-09 14:56:20 -0600

    improve search tile

--- lib/ui/search/search_tile.dart
+++ lib/ui/search/search_tile.dart
@@ -3,39 +3,84 @@ import 'package:medication_tracker/data/model/fda_drug_model.dart';
 
 class SearchTile extends StatelessWidget {
   final FDADrug drug;
+  final VoidCallback? onTap;
 
-  const SearchTile({super.key, required this.drug});
+  const SearchTile({super.key, required this.drug, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.2),
-            spreadRadius: 1,
-            blurRadius: 5,
-            offset: const Offset(0, 1),
-          ),
-        ],
-      ),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Text(
-              'Brand: ${drug.brandName} - Generic: ${drug.genericName}',
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 1,
+              blurRadius: 5,
+              offset: const Offset(0, 1),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            // Brand Name
+            Text(
+              drug.brandName,
               style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.w500,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
                 color: Colors.black,
               ),
             ),
-          ),
-        ],
+            const SizedBox(height: 4),
+
+            // Generic Name
+            Text(
+              drug.genericName,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                color: Colors.black,
+              ),
+              maxLines: 2, // Limit to 2 lines
+              overflow: TextOverflow.ellipsis, // Add ellipsis if text overflows
+            ),
+            const SizedBox(height: 8),
+
+            // Dosage Form and NDC
+            Row(
+              children: [
+                const Icon(Icons.medical_services,
+                    size: 16, color: Colors.grey),
+                const SizedBox(width: 4),
+                Text(
+                  drug.dosageForm,
+                  style: const TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(width: 16),
+                const Icon(Icons.confirmation_number,
+                    size: 16, color: Colors.grey),
+                const SizedBox(width: 4),
+                Text(
+                  'NDC: ${drug.ndc}',
+                  style: const TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
