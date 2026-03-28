b8e4d1eafbff84b2b348d13eda0ea1207a481d11
Dhruva Kumar <102192960+dhruva-dk@users.noreply.github.com>
2026-03-28 17:30:33 -0500

    sort medications by alphabetical order

--- lib/data/database/database.dart
+++ lib/data/database/database.dart
@@ -138,7 +138,8 @@ class DatabaseService {
   // Add methods for CRUD operations on profiles and medications
   Future<List<UserProfile>> getAllProfiles() async {
     Database db = await database;
-    final List<Map<String, dynamic>> maps = await db.query(profileTable);
+    final List<Map<String, dynamic>> maps =
+        await db.query(profileTable, orderBy: 'name COLLATE NOCASE');
     return List.generate(maps.length, (i) => UserProfile.fromMap(maps[i]));
   }
 
@@ -183,6 +184,7 @@ class DatabaseService {
       medicationTable,
       where: 'profile_id = ?',
       whereArgs: [profileId],
+      orderBy: 'name COLLATE NOCASE',
     );
     return List.generate(maps.length, (i) => Medication.fromMap(maps[i]));
   }
